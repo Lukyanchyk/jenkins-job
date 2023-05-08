455370eebb597ff7f9cc924435c7d03edd27bbea
Mikalai Lukyanchyk <Mikalai.Lukuanchyk@gmail.com>
2023-05-08 19:20:39 +0000

    Test seed

--- seed.groovy
+++ seed.groovy
@@ -1,20 +1,15 @@
-folder('TestFolder') {
-    displayName('TestFolder')
-    description('Folder for XML Seed Jobs')
-}
-
-job("TestFolder/get_xml"){
+job("get_xml"){
     scm {
         github('Lukyanchyk/jenkins-job', 'main')
     }
     steps {
         shell("""
-            ls -l /var/lib/jenkins/jobs/TestFolder/jobs/
-            rm -rf /var/lib/jenkins/jobs/TestFolder/jobs/*
+            ls -l /var/lib/jenkins/jobs/
+            rm -rf /var/lib/jenkins/jobs/*01
             pwd
             ls -l
-            mv /var/lib/jenkins/workspace/TestFolder/get_xml/jobs/* /var/lib/jenkins/jobs/TestFolder/jobs/
-            ls -l /var/lib/jenkins/jobs/TestFolder/jobs/
+            cp /var/lib/jenkins/workspace/get_xml/job/* /var/lib/jenkins/jobs/
+            ls -l /var/lib/jenkins/jobs/
         """)
     }
 }
